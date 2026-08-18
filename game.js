ac55b54f724d6f19af66da17a9e671d969b81e53
Angel Diaz <angel_diazm@hotmail.com>
2026-08-18 18:14:17 +0200

    Arreglo de bug

--- game.js
+++ game.js
@@ -261,6 +261,7 @@ function togglePause() {
 }
 
 function loop(ts) {
+  if (gameOver) return;
   const dt = ts - lastTime;
   lastTime = ts;
   dropAccum += dt;
@@ -273,6 +274,7 @@ function loop(ts) {
     }
   }
   draw();
+  if (gameOver) return;
   animId = requestAnimationFrame(loop);
 }
 
